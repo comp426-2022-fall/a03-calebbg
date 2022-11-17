faf3049f16975a3c27829ba310cee717f6633d8d
Caleb Gill <calebbg@UNC-R90YDFXE.localdomain>
2022-11-16 20:25:01 -0500

    Implemented CLI with node bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 import minimist from "minimist";
-import { roll } from "/lib/roll.js";
+import { roll } from "../lib/roll.js";
+
+const args = minimist(process.argv.slice(2));
+
+var num_sides = 6;
+if (args.sides) {
+    num_sides = args.sides;
+}
+
+var num_dice = 2;
+if (args.dice) {
+    num_dice = args.dice;
+}
+
+var num_rolls = 1;
+if (args.rolls) {
+    num_rolls = args.rolls;
+}
+
+let output = roll(num_sides, num_dice, num_rolls);
+
+console.log(JSON.stringify(output));
+
+// console.log(num_sides, num_dice, num_rolls);
